79e0854e9c60e697768b1953b821af43afe0dcdb
Divya <pai.divya03@gmail.com>
2025-04-22 15:43:55 +0530

    Ansible setup

---
+++ ansible/Install_ansible.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Ensure the script is run with root privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# Install Ansible & dependencies
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository -y --update ppa:ansible/ansible
+sudo apt install -y ansible
+ansible --version
+sudo apt install -y ansible-lint
+
+# Add localhost to default inventory
+LOCAL_CONN='localhost ansible_connection=local'
+ANSIBLE_HOST=/etc/ansible/hosts
+
+if ! grep -q $LOCAL_CONN $ANSIBLE_HOST; then
+    echo $LOCAL_CONN | sudo tee -a $ANSIBLE_HOST
+else
+    echo "$LOCAL_CONN already added to $ANSIBLE_HOST."   
+fi
+
+# Install SSH for ansible
+sudo apt install -y openssh-server
+sudo systemctl start ssh
+sudo systemctl enable ssh
+
+# Run the playbook
+ansible-playbook playbook.yml
